6d097935214cc7b689e325f5a545bcbd38af7a13
Yubraj <iyubrajpoudel@gmail.com>
2023-06-01 23:21:18 +0545

    Getting all books function defined in services, mapped and configured to controllel and main module. Tested and working for getting books when /books endpoint hitted.

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookSchema } from './schemas/book.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: "Book", schema: BookSchema}
+    ])
+    // make sure to register model in main module file inside imports
+  ],
+
   controllers: [BookController],
   providers: [BookService]
 })
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from "mongoose";
+
+// importing book schema
+import { Book } from './schemas/book.schema';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(
+        @InjectModel(Book.name)       
+        private bookModel: mongoose.Model<Book>
+        // bookModel will be used to do db operations like find(), findOne()...
+    ){}
+
+    async findAllBooks(): Promise<Book[]>{
+        const books = await this.bookModel.find();
+        return books;
+    }
+}
